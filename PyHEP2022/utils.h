e27962bb75bbbd3545c3584a84397ff7d39958f3
root <tedesk95@gmail.com>
2022-09-07 15:47:46 +0200

    update demo

--- PyHEP2022/utils.h
+++ PyHEP2022/utils.h
@@ -1,2 +1,277 @@
 #include "ROOT/RDataFrame.hxx"
 #include "ROOT/RVec.hxx"
+
+/// Header file with functions needed to execute the Python version of
+/// postselection step of the analysis. The header is declared to the
+/// ROOT C++ interpreter prior to the start of the analysis via the
+/// `ROOT.gInterpreter.Declare()` function.
+///
+
+#ifndef POST_H
+#define POST_H
+
+#include "ROOT/RDataFrame.hxx"
+#include "ROOT/RVec.hxx"
+#include "TCanvas.h"
+#include "TH1D.h"
+#include "TFile.h"
+#include "TH2D.h"
+#include "TLatex.h"
+#include "Math/Vector4D.h"
+#include "TStyle.h"
+#include <map>
+
+#include "TDavixFile.h"
+
+using namespace ROOT::VecOps;
+using RNode = ROOT::RDF::RNode;
+using rvec_f = const RVec<float> &;
+using rvec_i = const RVec<int> &;
+using rvec_b = const RVec<bool> &;
+
+//values for cuts and constant 
+
+const size_t ONLYELE=1;
+const size_t ONLYMU=0;
+
+const float PT_CUT_MU=  30;
+const float ETA_CUT_MU= 2.4;
+const float ISO_CUT_MU= 0.15;
+
+//const size_t PT_CUT_ELE=  35;
+const float PT_CUT_ELE_UL2016=  30;
+const float PT_CUT_ELE_UL2017=  38;
+const float PT_CUT_ELE_UL2018=  35;
+
+const float ETA_CUT_ELE= 2.4;
+const float ISO_CUT_ELE= 0.08;
+
+const float REL_ISO_CUT_LEP_VETO_ELE=   0.2;
+const float PT_CUT_LEP_VETO_ELE=        15;
+const float ETA_CUT_LEP_VETO_ELE=       2.4;
+
+const float REL_ISO_CUT_LEP_VETO_MU=    0.4;
+const float PT_CUT_LEP_VETO_MU=         10;
+const float ETA_CUT_LEP_VETO_MU=        2.4;
+
+const float DR_OVERLAP_CONE_TAU=        0.5;
+//const float DR_OVERLAP_CONE_TAU=        0.2;
+
+const float DR_OVERLAP_CONE_OTHER=      0.4;
+
+const float PT_CUT_JET= 30;
+const float ETA_CUT_JET=5;
+
+const float DELTAETA_JJ_CUT=2.5;
+
+const float BTAG_PT_CUT =   30;
+const float BTAG_ETA_CUT=   5;
+string BTAG_ALGO   =   "DeepFlv";
+string BTAG_WP     =   "M";
+const float BTAG_WP_VALUE = 0.3033;
+const float BTAG_WP_VALUE_LOOSE = 0.0521;
+
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_LOOSE_ELE_UL2016APV = 8;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_VETO_ELE_UL2016APV = 8;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_LOOSE_MU_UL2016APV = 4;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_VETO_MU_UL2016APV = 4;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_LOOSE_ELE_UL2016 = 4 ;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_VETO_ELE_UL2016 = 4;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_LOOSE_MU_UL2016 = 8;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_VETO_MU_UL2016 = 8;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_LOOSE_ELE_UL2017 = 16;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_VETO_ELE_UL2017 = 16;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_LOOSE_MU_UL2017 = 8;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_VETO_MU_UL2017 = 8;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_LOOSE_ELE_UL2018 = 16;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_VETO_ELE_UL2018 = 16;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_LOOSE_MU_UL2018 = 16;
+const size_t ID_TAU_RECO_DEEPTAU_VSJET_VETO_MU_UL2018 = 16;
+
+//const size_t ID_TAU_RECO_DEEPTAU_VSJET_VETO_ELE = 16;  //DUMMY!!!!!!!!
+//const size_t ID_TAU_RECO_DEEPTAU_VSJET_VETO_MU = 8; 
+
+//const size_t ID_TAU_RECO_DEEPTAU_VSJET_LOOSE_ELE = 16; //byDeepTau2017v2p1VSjet ID working points (deepTau2017v2p1): bitmask 1 = VVVLoose, 2 = VVLoose, 4 = VLoose, 8 = Loose, 16 = Medium, 32 = Tight, 64 = VTight, 128 = VVTight
+//const size_t ID_TAU_RECO_DEEPTAU_VSJET_LOOSE_MU = 8; //byDeepTau2017v2p1VSjet ID working points (deepTau2017v2p1): bitmask 1 = VVVLoose, 2 = VVLoose, 4 = VLoose, 8 = Loose, 16 = Medium, 32 = Tight, 64 = VTight, 128 = VVTight
+
+const size_t ID_TAU_RECO_DEEPTAU_VSJET=  64; //byDeepTau2017v2p1VSjet ID working points (deepTau2017v2p1): bitmask 1 = VVVLoose, 2 = VVLoose, 4 = VLoose, 8 = Loose, 16 = Medium, 32 = Tight, 64 = VTight, 128 = VVTight
+const size_t ID_TAU_RECO_DEEPTAU_VSELE=  4;  //byDeepTau2017v2p1VSe ID working points (deepTau2017v2p1): bitmask 1 = VVVLoose, 2 = VVLoose, 4 = VLoose, 8 = Loose, 16 = Medium, 32 = Tight, 64 = VTight, 128 = VVTight
+const size_t ID_TAU_RECO_DEEPTAU_VSMU=   8;  //byDeepTau2017v2p1VSmu ID working points (deepTau2017v2p1): bitmask 1 = VLoose, 2 = Loose, 4 = Medium, 8 = Tight
+
+const size_t ID_TAU_RECO_MVA=            8; //IsolationMVArun2v1DBoldDMwLT ID working point (2017v1): bitmask 1 = VVLoose, 2 = VLoose, 4 = Loose, 8 = Medium, 16 = Tight, 32 = VTight, 64 = VVTight
+const size_t ID_TAU_ANTIMU=              1; //Anti-muon discriminator V3: : bitmask 1 = Loose, 2 = Tight
+const size_t ID_TAU_ANTIELE=             2; //Anti-electron MVA discriminator V6 (2015): bitmask 1 = VLoose, 2 = Loose, 4 = Medium, 8 = Tight, 16 = VTight
+const float PT_CUT_TAU=30;
+const float ETA_CUT_TAU=2.3;
+const float M_JJ_CUT=   500;
+const float MET_CUT=    40;
+
+const string vsJetwp = "VTight";
+const string vsElewp = "VLoose";
+const string vsMuwp = "Tight";
+
+float deltaPhi (float phi1, float phi2){
+    float dphi = (phi1-phi2);
+    while(dphi >  M_PI) dphi -= 2*M_PI;
+    while(dphi < -M_PI) dphi += 2*M_PI;
+    return dphi;
+}
+
+float deltaR(float eta1, float phi1, float eta2, float phi2){
+    return hypot(eta1 - eta2, deltaPhi(phi1, phi2)); 
+}
+
+
+bool LepVetoMu(rvec_i Muon_idx, rvec_f Electron_pt, rvec_f Electron_eta, rvec_b Electron_mvaFall17V2Iso_WPL, rvec_f Electron_jetRelIso, rvec_f Muon_pt, rvec_f Muon_eta, rvec_f Muon_pfRelIso04_all, rvec_b Muon_looseId)
+{
+    bool IsEleVetoPassed = true;
+    bool IsMuVetoPassed = true;
+    for (size_t i = 0; i < Electron_pt.size(); i++) {
+        if(Electron_mvaFall17V2Iso_WPL[i] && Electron_pt[i] > PT_CUT_LEP_VETO_ELE && abs(Electron_eta[i]) < ETA_CUT_LEP_VETO_ELE && !(abs(Electron_eta[i])>1.4442 && abs(Electron_eta[i])<1.566) && Electron_jetRelIso[i] < REL_ISO_CUT_LEP_VETO_ELE) IsEleVetoPassed = false;
+    }
+    if(IsEleVetoPassed == true){
+        for (size_t i = 0; i < Muon_pt.size(); i++) {
+            if(i != Muon_idx[0] && Muon_looseId[i] && Muon_pt[i] > PT_CUT_LEP_VETO_MU && abs(Muon_eta[i]) < ETA_CUT_LEP_VETO_MU && Muon_pfRelIso04_all[i] < REL_ISO_CUT_LEP_VETO_MU) IsMuVetoPassed = false;
+        }
+    }
+    return IsEleVetoPassed && IsMuVetoPassed;
+}
+
+RVec<size_t> GoodJets(rvec_f eta, rvec_f pt, rvec_i puId){
+   RVec<int> idx;
+   for (size_t i = 0; i < pt.size(); i++) {
+      if (abs(eta[i]) < 5. && pt[i] > PT_CUT_JET && (pt[i] > 50. || (pt[i] <= 50. && puId[i] >= 7))) idx.emplace_back(i);
+   }
+   return idx;
+}
+
+bool atleast2GoodJets(rvec_i GoodJets_idx){
+    if (GoodJets_idx.size() >= 2) return true;
+    else return false;
+}
+
+
+RVec<size_t> SelectVBSJets_invmass(rvec_f pt, rvec_f eta, rvec_f phi, rvec_f mass, rvec_i GoodJets_idx)
+{
+    
+    RVec<size_t> idx;
+    // Find first lepton pair with invariant mass closest to Z mass
+    auto idx_cmb = Combinations(GoodJets_idx, 2);
+    //auto best_mass = -1.;
+    float best_mass = -1.;
+    size_t best_i1 = 0; size_t best_i2 = 0;
+    for (size_t i = 0; i < idx_cmb[0].size(); i++) {
+        const auto i1 = idx_cmb[0][i];
+        const auto i2 = idx_cmb[1][i];
+        //cout<<i1<<i2<<endl;
+        if (abs(eta[GoodJets_idx[i1]] - eta[GoodJets_idx[i2]]) >= DELTAETA_JJ_CUT) {
+            ROOT::Math::PtEtaPhiMVector p1(pt[GoodJets_idx[i1]], eta[GoodJets_idx[i1]], phi[GoodJets_idx[i1]], mass[GoodJets_idx[i1]]);
+            ROOT::Math::PtEtaPhiMVector p2(pt[GoodJets_idx[i2]], eta[GoodJets_idx[i2]], phi[GoodJets_idx[i2]], mass[GoodJets_idx[i2]]);
+            //const auto this_mass = (p1 + p2).M();
+            float this_mass = (p1 + p2).M();
+            //cout<<this_mass<<endl;
+            if (this_mass > best_mass) {
+                best_mass = this_mass;
+                best_i1 = GoodJets_idx[i1];
+                best_i2 = GoodJets_idx[i2];
+            }
+        }
+    } 
+    //cout<<"best "<<best_i1<<best_i2<<best_mass;
+    idx.emplace_back(best_i1);
+    idx.emplace_back(best_i2);
+    return idx;
+}
+
+float GetInvMassNoIndex4(float pt1, float eta1, float phi1, float mass1, float pt2, float eta2, float phi2, float mass2, float pt3, float eta3, float phi3, float mass3, float pt4, float eta4, float phi4, float mass4)
+{
+    ROOT::Math::PtEtaPhiMVector p1(pt1, eta1, phi1, mass1);
+    ROOT::Math::PtEtaPhiMVector p2(pt2, eta2, phi2, mass2);
+    ROOT::Math::PtEtaPhiMVector p3(pt3, eta3, phi3, mass3);
+    ROOT::Math::PtEtaPhiMVector p4(pt4, eta4, phi4, mass4);
+    return (p1 + p2 + p3 + p4).M();
+}
+
+
+RVec<int> SelectMuon(rvec_f lepton_pt, rvec_f lepton_eta, rvec_f lepton_phi, rvec_f jet_eta, rvec_f jet_phi, rvec_i VBSJets_idx){
+    
+    float jet1_idx = VBSJets_idx[0];
+    float jet2_idx = VBSJets_idx[1];
+    float jet1eta = jet_eta[jet1_idx];
+    float jet2eta = jet_eta[jet2_idx];
+    float jet1phi = jet_phi[jet1_idx];
+    float jet2phi = jet_phi[jet2_idx];
+    
+    const float isocone = DR_OVERLAP_CONE_OTHER;
+
+    RVec<size_t> Tleptons_idx;
+    RVec<size_t> LnTleptons_idx;
+    
+    bool IsInEtaRegion, IsInPtRegion;
+    
+    for (size_t i = 0; i < lepton_pt.size(); i++) {
+        //setting loose and tight, eta, and pt criteria for leptons depending on lepton flavour
+        IsInEtaRegion = abs(lepton_eta[i]) < ETA_CUT_MU;
+        IsInPtRegion = lepton_pt[i] > PT_CUT_MU;
+        
+        //find tight and loose-not-tight leptons filtering with jet-lep isolation criteria
+        if (IsInEtaRegion && IsInPtRegion){
+            if(deltaR(lepton_eta[i], lepton_phi[i], jet1eta,  jet1phi) > isocone && deltaR(lepton_eta[i], lepton_phi[i], jet2eta,  jet2phi) > isocone){
+                    Tleptons_idx.emplace_back(i);
+            }
+        }
+    }
+    RVec<int> idx(2);
+    //select leading tight/loose-not-tight lepton
+    if (Tleptons_idx.size() > 0){
+        idx[0] = Tleptons_idx[0];
+        idx[1] = 1;
+    }
+    else{
+        idx[0] = -1;
+        idx[1] = -1;  
+    }
+    
+    return idx;
+}
+
+RVec<int> SelectAndVetoTaus(rvec_f Tau_pt, rvec_f Tau_eta, rvec_f Tau_phi, rvec_i Muon_idx, rvec_f Muon_eta, rvec_f Muon_phi, rvec_f Jet_eta, rvec_f Jet_phi, rvec_i VBSJet_idx)
+{
+    float jet1eta = Jet_eta[VBSJet_idx[0]];
+    float jet2eta = Jet_eta[VBSJet_idx[1]];
+    float jet1phi = Jet_phi[VBSJet_idx[0]];
+    float jet2phi = Jet_phi[VBSJet_idx[1]];
+    float isocone = DR_OVERLAP_CONE_OTHER;
+
+    size_t nTau=0;
+    RVec<int> idx(2);
+    
+    if (Tau_eta.size()==0){
+        idx[0] = -1;
+        idx[1] = -1;
+        return idx;
+    } 
+
+    for (size_t i = 0; i < Tau_eta.size(); i++) {
+        if (deltaR(Tau_eta[i], Tau_phi[i], Muon_eta[Muon_idx[0]], Muon_phi[Muon_idx[0]])>DR_OVERLAP_CONE_TAU && deltaR(Tau_eta[i], Tau_phi[i], jet1eta, jet1phi)>isocone && deltaR(Tau_eta[i], Tau_phi[i], jet2eta, jet2phi)>isocone && Tau_pt[i] >=PT_CUT_TAU && abs(Tau_eta[i])<=ETA_CUT_TAU){
+                    nTau++;
+                    idx[0] = i;
+                    idx[1] = 1;
+        }
+    }
+    if(nTau!=1) idx[1] = -1;                                                                                               
+    return idx;
+}
+
+bool SameCharge(int GoodLeptonFamily, rvec_i Electron_idx, rvec_i Electron_charge, rvec_i Muon_idx, rvec_i Muon_charge, rvec_i Tau_idx, rvec_i Tau_charge){
+    if(GoodLeptonFamily == 0){
+        if(Electron_charge[Electron_idx[0]] == Tau_charge[Tau_idx[0]]) return true;
+        else return false;
+    }
+    else if(GoodLeptonFamily == 1){
+        if(Muon_charge[Muon_idx[0]] == Tau_charge[Tau_idx[0]]) return true;
+        else return false;
+    }
+    return false;
+}
+#endif
